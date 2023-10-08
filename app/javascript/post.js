854a8a170e5a6261556f619184b35a9f2ada7c7a
okmon12 <143723986+okmon12@users.noreply.github.com>
2023-10-08 17:28:56 +0900

    レスポンス完了

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -7,6 +7,22 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const item = XHR.response.article;
+      const contentsArea = document.getElementById("contents_area");
+      const articleText = document.getElementById("article_text");
+      const HTML = `
+        <div class="article">
+          ${ item.text }
+        </div>`;
+      contentsArea.insertAdjacentHTML("afterbegin", HTML);
+      articleText.value = "";
+      
+      const charNum  = document.getElementById("char_num");
+      charNum.innerHTML = "0文字";
     };
     e.preventDefault();
   });
